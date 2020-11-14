c32611b32ab23d3cbdc9560e827616fcc0f1e6ab
Igor Krasheninnikov <igor.krash@gmail.com>
2020-11-14 18:16:20 +0300

    + db_controller"
    Signed-off-by: Igor Krasheninnikov <igor.krash@gmail.com>

--- api/routers/indexer.py
+++ api/routers/indexer.py
@@ -1,38 +1,52 @@
 from fastapi import APIRouter
 import requests
 import pandas as pd
+from api.utils import db_controller
 
 router = APIRouter()
 
 
 @router.get("/bot/v1/index/all")
 def index_all():
-    df = pd.read_csv("../data/KB.csv")
+    db_controller.prep()
+    db_controller.init()
+    df = pd.read_csv("../KB.csv")
     for index, row in df.iterrows():
-        print(row.index)
+        # print(row['Вопрос'])
         bert_body = {
-            "id": 0,
-            "texts": [[row['Вопрос']]],
-            "is_tokenized": True,
+            "id": index,
+            "texts": [row['Вопрос']],
+            "is_tokenized": False,
         }
-        vector = requests.post("http://127.0.0.1:8125/encode", json=bert_body).json()['result']
-        # TODO: Добавление в БД
-        break
-    pass
+        print(bert_body)
+        vector = requests.post("http://10.241.1.250:8125/encode", json=bert_body).json()['result']
+        db_controller.cursor.execute("INSERT INTO vectors VALUES(%s, %s)", (index, vector[0]))
+    db_controller.connection.commit()
 
 
 @router.post("/bot/v1/index/new")
 async def index_one(body: dict):
+    db_controller.cursor.execute("SELECT index FROM knowledge_base ORDER BY index DESC LIMIT 1")
+    max_index = int(db_controller.cursor.fetchone()[0])
+    db_controller.cursor.execute("INSERT INTO knowledge_base VALUES(%s, %s, %s, %s, %s, %s)", (max_index + 1,
+                                                                                               body['request'],
+                                                                                               body['clarification'],
+                                                                                               body['category'],
+                                                                                               body['question'],
+                                                                                               body['answer']))
+    db_controller.connection.commit()
     id = 1
-    is_tokenized = True
+    is_tokenized = False
     bert_body = {
         "id": id,
-        "texts": [[body["question"]]],
+        "texts": [body["question"]],
         "is_tokenized": is_tokenized
     }
     r = requests.post("http://127.0.0.1:8125/encode", json=bert_body)
     if r.status_code == 200:
         vector = r.json()['result']
-        # TODO: Добавление в БД
+        db_controller.cursor.execute("INSERT INTO vectors VALUES(%s, %s)", (max_index + 1, [0]))
         return {"vector": vector}
     return r.raise_for_status()
+
+index_all()
